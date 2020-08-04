a154a3a36cf6f3970061ea69765437f98325dc64
no-name-no-idea <37925591+no-name-no-idea@users.noreply.github.com>
2020-08-04 12:06:17 +0900

    Update 전화번호 목록.py

--- Python algorithm/전화번호 목록.py
+++ Python algorithm/전화번호 목록.py
@@ -1,8 +1,10 @@
 def solution(phone_book):
     phone_book.sort()
     
-    for p1, p2 in zip(phone_book, phone_book[1:]):
-        if p2.startswith(p1):
-            return False
-    
+    for phone_num in phone_book:
+        temp = ''
+        for num in phone_num:
+            temp += num
+            if temp in phone_book and temp != phone_num:
+                return False
     return True
